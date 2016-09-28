bc5047e38a6e4090831b40ab871318800439f52b
jkdragonite <Jordan@Laptop-PC>
2016-09-28 18:59:03 +1300

    Small example of move retrieval

--- src/game/IntegrationDemo.java
+++ src/game/IntegrationDemo.java
@@ -1,13 +1,23 @@
 package game;
 
+import java.util.ArrayList;
+
 public class IntegrationDemo {
 	
 	public static void main(String args[]){
+		
+		// create instance of game
 		Game game = new Game();
+	
+		// adds a player to the game
+		game.addPlayer("player 1");
 		
-		// add player
+		// empty arraylist of locations
+		ArrayList<Location> playerMoves = new ArrayList<>();
 		
-		// add solution
+		// retrieves board from a room, uses player to creat list of possible moves
+		// this will be propperly implemented in the game class
+		playerMoves = game.rooms.get(0).board.possibleMoves(game.players.get(0));
 		
 		
 		
