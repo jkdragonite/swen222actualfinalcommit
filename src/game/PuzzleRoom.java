97ab13a4187d4eadfcac92ad7ade1c33f5ca81d3
jkdragonite <Jordan@Laptop-PC>
2016-09-28 16:34:20 +1300

    Added item and player placement to puzzle room

--- src/game/PuzzleRoom.java
+++ src/game/PuzzleRoom.java
@@ -7,8 +7,31 @@ public class PuzzleRoom extends Room {
 	public PuzzleRoom(int size){
 		this.board = new Board(size);
 		
-		
 	}
+	
+	/**
+	 * Places an instance of a player class at a given location
+	 * 
+	 * @param player
+	 * @param location
+	 */
+	public void placePlayer(Player player, Location location){
+		this.board.grid[location.getY()][location.getX()].addPlayer(player);
+	}
+	
+	
+	/**
+	 * 
+	 * places item at given grid location
+	 * 
+	 * 
+	 * @param item
+	 * @param location
+	 */
+	public void PlaceItem(Item item, Location location){
+		this.board.grid[location.getY()][location.getX()].setItem(item);
+	}
+	
 	
 	
 }
