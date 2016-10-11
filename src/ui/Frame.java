91c4145beae0835ec80e8b568b0d556cfd359cf8
brooke-meleah <brooke.meleah.42@gmail.com>
2016-10-11 23:41:43 +1300

    frame javadoc

--- src/ui/Frame.java
+++ src/ui/Frame.java
@@ -18,11 +18,23 @@ import javax.swing.JPopupMenu;
 import game.Game;
 import render.GameRenderer;
 
+
+/**
+ * The frame class holds the components and panels necessary to 
+ * render the game and all of it's UI. This is created by the client, 
+ * and interacts with a given game object and the rendering package.
+ * The frame is unique to each player, displaying the info relevant
+ * to them as well as logging their movements so that they can be 
+ * reflected back to the server version of the game.
+ * 
+ * @author Josh 300278912
+ *
+ */
 public class Frame extends JFrame implements  ActionListener {
 
 	//unique player ID
 	public static int playerID;
-	
+
 	// panel fields
 	public InventoryPanel ip;
 	public RenderPanel rp;
@@ -32,15 +44,25 @@ public class Frame extends JFrame implements  ActionListener {
 	JMenu menu;
 	JMenu help;
 	JMenu save;
-	
+
 	// the game
 	private Game theGame;
-   
-	   public Frame(String title,Game g,int UID) {
-    	super(title);
-    	theGame = g;
-    	playerID = UID;
-    	    	
+
+	/**
+	 * Constructor for the frame - called by the client with the title to be
+	 * displayed on the top, the game object to manipulate, and the unique
+	 * identification code that tells the frame which player to render from 
+	 * the perspective of.
+	 * 
+	 * @param title
+	 * @param g
+	 * @param UID
+	 */
+	public Frame(String title,Game g,int UID) {
+		super(title);
+		theGame = g;
+		playerID = UID;
+
 		// Menu bar things
 		menuBar = new JMenuBar();
 		this.setJMenuBar(menuBar);
@@ -59,7 +81,7 @@ public class Frame extends JFrame implements  ActionListener {
 		showHelp.setActionCommand("showHelp");
 		JMenuItem saveGame = new JMenuItem("Save game");
 		JMenuItem loadGame = new JMenuItem("Load game");
-		
+
 		MenuItemListener menuItemListener = new MenuItemListener();
 
 		start.addActionListener(menuItemListener);
@@ -77,30 +99,22 @@ public class Frame extends JFrame implements  ActionListener {
 
 		setLayout(new BorderLayout());
 
-		
-
-
-		
-		
 		rp = new RenderPanel(theGame, playerID);
 		add(rp, BorderLayout.CENTER);
-		
+
 		ip = new InventoryPanel(theGame,playerID,rp);
 		add(ip, BorderLayout.SOUTH);
-		
-		
+
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setResizable(false);
 		setVisible(true);
 	}
 
-	public static void main(String[] args) {
-		//new Frame("testFrame", new KeyListener(););
-	}
-
 	/*
-	 * Class to make the menu actions happen
+	 * the MenuListener class is an implementation of ActionListener
+	 * that manages the menu bar and processes the input there.
 	 */
 	class MenuItemListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -113,18 +127,19 @@ public class Frame extends JFrame implements  ActionListener {
 				System.exit(0);
 			}
 			if (e.getActionCommand().equals("showHelp")) {
-			//make a new help frame pop up
-			new HelpFrame();
-				
+				//make a new help frame pop up
+				new HelpFrame();
+
 			}
 
 		}
 	}
 
+	/**
+	 * This is a stub method - the actual action performed logic happens
+	 * on the panel which the action happens on.
+	 */
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void actionPerformed(ActionEvent e) {}
 
 }
